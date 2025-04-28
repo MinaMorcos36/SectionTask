5b9c642f6c938bcdab38c9a8a2a048b58d5b966b
Mina Morcos <minanew0@gmail.com>
2025-04-29 01:09:47 +0300

    updated the title to Github

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Github',
       theme: ThemeData(
         // This is the theme of your application.
         //
